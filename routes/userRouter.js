477b7a2dded1002167651c1373eaf96f1b51df80
Sandun Kumara <sandun89@gmail.com>
2025-06-14 10:33:43 +0530

    Registration page completed 👍👍

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getAllUsers, loginUser, saveUser } from "../controllers/userController.js";
+import {
+  getAllUsers,
+  loginUser,
+  saveUser,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", saveUser); //save user
-userRouter.post("/login", loginUser);   //login user
+userRouter.post("/login", loginUser); //login user
 userRouter.get("/all", getAllUsers); //get all users
 
 export default userRouter;
