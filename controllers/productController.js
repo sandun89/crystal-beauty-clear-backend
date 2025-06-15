5358b4b01d8f1afddb8d91401a6eb72a8446b8ab
Sandun Kumara <sandun89@gmail.com>
2025-06-15 16:02:16 +0530

    backend update 1️⃣

--- controllers/productController.js
+++ controllers/productController.js
@@ -44,6 +44,21 @@ export function getProducts(req, res) {
     });
 }
 
+export async function getProductById(req, res) {
+  const productId = req.params.productId;
+  const product = await Product.findOne({ productId: productId });
+  if (product == null) {
+    res.status(404).json({
+      message: "Product Not Found",
+    });
+    return;
+  }
+
+  res.json({
+    product: product,
+  });
+}
+
 export function deleteProduct(req, res) {
   //check user logged
   if (req.user == null) {
